74f522460a2a43d6bb55fe237ce888c9ef2c3ed7
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-03-02 08:39:20 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Just to start with a new DB
+redis-cli flushdb
+
 for item in 0 1 2 3 4 5 6 7
 do
     curl -d "{\"data\":\"Note $item\"}" -H "Content-Type: application/json" -X POST http://localhost:12345/notes
@@ -10,4 +13,7 @@ curl -X GET http://localhost:12345/notes
 echo
 
 curl -X GET http://localhost:12345/notes/1
+echo
+
+curl -X GET http://localhost:12345/notes/azer
 echo
