78ff985e015614ea0864ffb88d76bb058c6d132d
Sylvain METAYER <sylvain_metayer@outlook.fr>
2018-03-16 13:12:35 +0100

    Add linter / codecov / update CI

--- index.js
+++ index.js
@@ -1,88 +1,89 @@
-const express = require('express');
-const app = express();
-let redis = require("redis");
-let client = redis.createClient();
-let bluebird = require("bluebird");
-let bodyParser = require("body-parser");
-let async = require("async");
+const express = require('express')
+const app = express()
+let redis = require('redis')
+let client = redis.createClient()
+let bluebird = require('bluebird')
+let bodyParser = require('body-parser')
+let async = require('async')
 
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/notes', (req, res) => {
-    var data = req.body.data;
-    client.getAsync("count").then((resultat) => {
-        var noteId = resultat;
-        var note = "notes:" + noteId;
-        client.multi().set(note, data).incr("count").execAsync().then((result) => {
-            res.send(noteId);
-        });
-    });
-});
+  var data = req.body.data
+  client.getAsync('count').then((resultat) => {
+    var noteId = resultat
+    var note = 'notes:' + noteId
+    client.multi().set(note, data).incr('count').execAsync().then((result) => {
+      res.send(noteId)
+    })
+  })
+})
 
-app.get("/notes", (req, res) => {
-    client.getAsync("count").then((count) => {
-        notes = [];
-        for (i = 0; i < count; i++) {
-            var note = "notes:" + i;
-            notes.push(note);
-        }
-
-        notes_data = [];
-        async.each(notes, (item, callback) => {
-            client.getAsync(item).then((note_data) => {
-                var obj = {};
-                obj[item] = note_data;
-                notes_data.push(obj);
-            }).then(() => {
-                callback();
-            });
-        }, (err) => {
-            res.send(JSON.stringify(notes_data));
-        });
-    });
-});
-
-app.get("/notes/:id", (req, res) => {
-    var id = parseInt(req.params.id);
-    if (!Number.isInteger(id)) {
-        res.send({ "error": "expect integer, got " + id });
-        return;
+app.get('/notes', (req, res) => {
+  client.getAsync('count').then((count) => {
+    let notes = []
+    for (let i = 0; i < count; i++) {
+      var note = 'notes:' + i
+      notes.push(note)
     }
 
-    client.getAsync("notes:" + id).then((note_data) => {
-        var obj = {};
-        obj["notes:" + id] = note_data;
-        res.send(obj);
-    });
-});
+    let notesData = []
+    async.each(notes, (item, callback) => {
+      client.getAsync(item).then((noteData) => {
+        var obj = {}
+        obj[item] = noteData
+        notesData.push(obj)
+      }).then(() => {
+        callback()
+      })
+    }, (err) => {
+      if (err) throw err
+      res.send(JSON.stringify(notesData))
+    })
+  })
+})
 
-client.on("error", () => {
-    console.log("Error");
-    console.log(err);
-});
+app.get('/notes/:id', (req, res) => {
+  var id = parseInt(req.params.id)
+  if (!Number.isInteger(id)) {
+    res.send({ 'error': 'expect integer, got ' + id })
+    return
+  }
 
-var server = undefined;
+  client.getAsync('notes:' + id).then((noteData) => {
+    var obj = {}
+    obj['notes:' + id] = noteData
+    res.send(obj)
+  })
+})
+
+client.on('error', (err) => {
+  console.log('Error')
+  console.log(err)
+})
+
+let server
 let start = () => {
-    server = app.listen(process.env.PORT, () => {
-        console.log(`'Note' listening on port ${process.env.PORT} !`);
-        client.setAsync("count", 0).then(() => {
-            console.log("Counter initialized");
-        });
-    });
+  server = app.listen(process.env.PORT, () => {
+    console.log(`'Note' listening on port ${process.env.PORT} !`)
+    client.setAsync('count', 0).then(() => {
+      console.log('Counter initialized')
+    })
+  })
 }
 
 let stop = () => {
-    server.close();
-    client.quit();
+  server.close()
+  client.quit()
 }
 
 if (require.main === module) {
-    start();
+  start()
 }
 
-module.exports.start = start;
-module.exports.close = stop;
+module.exports.start = start
+module.exports.close = stop
--- test.js
+++ test.js
@@ -1,82 +1,81 @@
-const should = require('chai').should;
-const assert = require("chai").assert;
-const expect = require("chai").expect;
-const request = require("supertest");
-const notes_module = require("./index");
-const redis = require("redis");
+const expect = require('chai').expect
+const request = require('supertest')
+const nodesModule = require('./index')
+const redis = require('redis')
 
-describe("Testing server...", () => {
-    var endpoint = 'http://localhost:12345';
+const { it, describe, after, before } = require('mocha')
 
-    before((done) => {
-        let client = redis.createClient();
-        client.FLUSHDB((err, reply) => {
-            if (err) throw err;
-            notes_module.start();
-            client.quit();
-            done();
-        });
-    });
+describe('Testing server...', () => {
+  var endpoint = 'http://localhost:12345'
 
-    after((done) => {
-        notes_module.close();
-        done();
-    });
+  before((done) => {
+    let client = redis.createClient()
+    client.FLUSHDB((err, reply) => {
+      if (err) throw err
+      nodesModule.start()
+      client.quit()
+      done()
+    })
+  })
 
-    it("Should add an element", (done) => {
-        var note = {
-            data: "A note"
-        };
+  after((done) => {
+    nodesModule.close()
+    done()
+  })
 
-        request(endpoint)
-            .post('/notes')
-            .send(note)
-            .end((err, res) => {
-                if (err) throw err;
-                expect(res.status).equal(200);
-                expect(res.text).equal("0");
-                done();
-            });
-    });
+  it('Should add an element', (done) => {
+    var note = {
+      data: 'A note'
+    }
 
-    it("Should list all elements", (done) => {
-        expected = "[{\"notes:0\":\"A note\"}]";
-        // The first note created
-        request(endpoint)
-            .get("/notes")
-            .send()
-            .end((err, res) => {
-                if (err) throw err;
-                expect(res.status).equal(200);
-                expect(res.text).equal(expected);
-                done();
-            });
-    });
+    request(endpoint)
+      .post('/notes')
+      .send(note)
+      .end((err, res) => {
+        if (err) throw err
+        expect(res.status).equal(200)
+        expect(res.text).equal('0')
+        done()
+      })
+  })
 
-    it("Should get one element", (done) => {
-        expected = "{\"notes:0\":\"A note\"}";
-        // The first note created
-        request(endpoint)
-            .get("/notes/0")
-            .send()
-            .end((err, res) => {
-                if (err) throw err;
-                expect(res.status).equal(200);
-                expect(res.text).equal(expected);
-                done();
-            });
-    });
+  it('Should list all elements', (done) => {
+    var expected = '[{"notes:0":"A note"}]'
+    // The first note created
+    request(endpoint)
+      .get('/notes')
+      .send()
+      .end((err, res) => {
+        if (err) throw err
+        expect(res.status).equal(200)
+        expect(res.text).equal(expected)
+        done()
+      })
+  })
 
-    it("Should get an invalid element", (done) => {
-        expected = "{\"error\":\"expect integer, got NaN\"}";
-        request(endpoint)
-            .get("/notes/invalid")
-            .send()
-            .end((err, res) => {
-                if (err) throw err;
-                expect(res.text).equal(expected);
-                done();
-            });
-    });
+  it('Should get one element', (done) => {
+    var expected = '{"notes:0":"A note"}'
+    // The first note created
+    request(endpoint)
+      .get('/notes/0')
+      .send()
+      .end((err, res) => {
+        if (err) throw err
+        expect(res.status).equal(200)
+        expect(res.text).equal(expected)
+        done()
+      })
+  })
 
-});
+  it('Should get an invalid element', (done) => {
+    var expected = '{"error":"expect integer, got NaN"}'
+    request(endpoint)
+      .get('/notes/invalid')
+      .send()
+      .end((err, res) => {
+        if (err) throw err
+        expect(res.text).equal(expected)
+        done()
+      })
+  })
+})
